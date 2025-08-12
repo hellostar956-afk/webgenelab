f1977bd91cab1a3395ad5bd4c06d922782096079
hellostar956-afk <hellostar956@gmail.com>
2025-08-12 11:19:38 +0500

    Fix useEffect import error

--- src/components/GeneLibrary.tsx
+++ src/components/GeneLibrary.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BookOpen, Share2, Download, Trash2, Eye, Plus, Beaker } from 'lucide-react';
 
 interface SavedExperiment {
